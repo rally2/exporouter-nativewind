69b64a5cf72ae024f08b0f94d6fcc103b02a1df6
Shane Keney <sckeney@gmail.com>
2023-09-22 19:57:23 -0400

    chore: Svg transformer & easier leveraging of Svg in project

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from 'react-native';
+import Svg from '../assets/Svg';
 
 export default function Page() {
   return (
@@ -6,6 +7,7 @@ export default function Page() {
       <View className={'flex flex-grow justify-center max-w-screen-lg mx-auto'}>
         <Text className={'text-6xl font-bold'}>Hello World</Text>
         <Text className="text-4xl">This is the first page of your app.</Text>
+        <Svg.Headphones height={50} width={50} />
       </View>
     </View>
   );
---
+++ assets/Svg/index.tsx
@@ -0,0 +1,7 @@
+import Headphones from './headphones.svg';
+
+const Svg = {
+  Headphones: Headphones
+};
+
+export default Svg;
