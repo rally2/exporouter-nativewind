bac01e804379424a71021eb17ab97e3aeb62d5b9
Shane Keney <shane.keney@curaleaf.com>
2023-10-11 00:09:58 -0400

    fix: type path aliases

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['@babel/plugin-proposal-export-namespace-from', 'react-native-reanimated/plugin', require.resolve('expo-router/babel'), 'nativewind/babel']
+    plugins: [
+      '@babel/plugin-proposal-export-namespace-from',
+      'react-native-reanimated/plugin',
+      require.resolve('expo-router/babel'),
+      'nativewind/babel'
+    ]
   };
 };
--- components/atoms/DemoPathAliasText/index.tsx
+++ components/atoms/DemoPathAliasText/index.tsx
@@ -2,5 +2,5 @@ import { Text } from 'react-native';
 import React from 'react';
 
 export default function DemoPathAliasText() {
-  return <Text className="bg-red-500">See example path alias</Text>;
+  return <Text className="bg-red-500 text-xl">See example path alias</Text>;
 }
