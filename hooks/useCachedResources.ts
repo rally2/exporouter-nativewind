da04445d390f7e105592e4577fe93a4948bdb4d0
Shane Keney <shane.keney@curaleaf.com>
2023-10-11 21:53:52 -0400

    feat: add custom font as an example & create custom hook that can be used for loading them as needed

---
+++ hooks/useCachedResources.ts
@@ -0,0 +1,52 @@
+import { useCallback, useEffect, useState } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary
+} from 'expo-router';
+
+const useCachedResources = () => {
+  const [appIsReady, setAppIsReady] = useState(false);
+  const [fontsLoaded, error] = useFonts({
+    'racesport-regular': require('../assets/fonts/RaceSport.ttf')
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        // await new Promise((resolve) => setTimeout(resolve, 5000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    if (fontsLoaded) prepare();
+  }, [fontsLoaded]);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  return { appIsReady, onLayoutRootView };
+};
+
+export default useCachedResources;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   content: [
     './App.{js,jsx,ts,tsx}',
     './app/**/*.{js,jsx,ts,tsx}',
-    './components/**/*.{js,jsx,ts,tsx}'
+    './components/**/*.{js,jsx,ts,tsx}',
+    './hooks/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {}
