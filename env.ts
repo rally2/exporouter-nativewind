a60d656b5f75a9b00b7b6e62083fae2b35d7e8a9
Shane Keney <shane.keney@curaleaf.com>
2023-10-14 23:06:12 -0400

    chore: add comments for env setup

--- env.ts
+++ env.ts
@@ -1,4 +1,4 @@
-// Envs are defined in app.json under the "extra" key
+// Envs are defined in app.json and injected at build time to "extra" key
 import Constants from 'expo-constants';
 const ENV = Constants.expoConfig?.extra ?? {};
 export default ENV;
