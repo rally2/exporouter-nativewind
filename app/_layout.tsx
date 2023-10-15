9ced9299736b6f39564d39fedce4517cf583deeb
Shane Keney <shane.keney@curaleaf.com>
2023-10-14 22:54:46 -0400

    feat: added root safe area context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import DemoPathAliasText from '@/components/atoms/DemoPathAliasText';
 import useCachedResources from '@/hooks/useCachedResources';
 import { SplashScreen } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -13,17 +14,19 @@ export default function RootLayout() {
   if (!appIsReady) return null;
 
   return (
-    <View className={'flex flex-1 items-center p-6'} onLayout={onLayoutRootView}>
-      <View className={'flex flex-grow justify-center max-w-screen-lg mx-auto'}>
-        <Text className={'text-6xl font-bold'}>Hello World</Text>
-        <Text className="text-4xl">This is the first page of your app.</Text>
-        <DemoPathAliasText />
-        <View className="flex-row justify-center items-center my-2">
-          <Text className="text-xl">Using an SVG:</Text>
-          <SVG.Headphones className="mx-4" height={25} width={25} />
+    <SafeAreaProvider onLayout={onLayoutRootView}>
+      <View className={'flex flex-1 items-center p-6'}>
+        <View className={'flex flex-grow justify-center max-w-screen-lg mx-auto'}>
+          <Text className={'text-6xl font-bold'}>Hello World</Text>
+          <Text className="text-4xl">This is the first page of your app.</Text>
+          <DemoPathAliasText />
+          <View className="flex-row items-center justify-center my-2">
+            <Text className="text-xl">Using an SVG:</Text>
+            <SVG.Headphones className="mx-4" height={25} width={25} />
+          </View>
+          <Text className="my-4 text-center font-racesport">RaceSport Custom Font</Text>
         </View>
-        <Text className="my-4 font-racesport text-center">RaceSport Custom Font</Text>
       </View>
-    </View>
+    </SafeAreaProvider>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './App.{js,jsx,ts,tsx}',
     './app/**/*.{js,jsx,ts,tsx}',
     './components/**/*.{js,jsx,ts,tsx}',
     './hooks/**/*.{js,jsx,ts,tsx}'
