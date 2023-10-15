e9eba05cf38cf2e3db76d8d62cf32bc61addd655
Shane Keney <shane.keney@curaleaf.com>
2023-10-14 23:14:50 -0400

    remove env.ts.  No longer needed due to full .env support post SDK49

--- env.ts
+++
@@ -1,4 +0,0 @@
-// Envs are defined in app.json and injected at build time to "extra" key
-import Constants from 'expo-constants';
-const ENV = Constants.expoConfig?.extra ?? {};
-export default ENV;
