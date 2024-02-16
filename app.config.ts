967c01a32c387befb706acd56e585af944744e6c
Shane Keney <shane.keney@curaleaf.com>
2024-02-16 16:41:58 -0500

    feat: env tweaks in favor of dyanmic config

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,20 @@
 import { ExpoConfig, ConfigContext } from '@expo/config';
 
 export const ENV = {
-  DYNAMIC_CONFIG_ENV_VAR: 'keep it secret, keep it safe... (dev)'
+  EXPO_PUBLIC_APP_DISPLAY_NAME: process.env.EXPO_PUBLIC_APP_DISPLAY_NAME || '(Dev) RouterWind',
+  EXPO_PUBLIC_PACKAGE: process.env.EXPO_PUBLIC_PACKAGE || 'com.rally2.routerwind.dev',
+  EXPO_PUBLIC_FOREGROUND_IMAGE:
+    process.env.EXPO_PUBLIC_FOREGROUND_IMAGE || './assets/dev/adaptive-icon.dev.png',
+  EXPO_PUBLIC_SCHEME: process.env.EXPO_PUBLIC_SCHEME || 'routerwinddev',
+  EXPO_PUBLIC_BUNDLE_ID: process.env.EXPO_PUBLIC_BUNDLE_ID || 'com.rally2.routerwind.dev',
+  EXPO_PUBLIC_ICON_PATH: process.env.EXPO_PUBLIC_ICON_PATH || './assets/dev/icon.dev.png',
+
+  // Versioing
+  EXPO_PUBLIC_APP_VERSION: process.env.EXPO_PUBLIC_APP_VERSION || '1.0',
+  EXPO_PUBLIC_BUILD_NUMBER: process.env.EXPO_PUBLIC_BUILD_NUMBER || '1',
+  EXPO_PUBLIC_OTA: process.env.EXPO_PUBLIC_OTA || '0',
+  EXPO_PUBLIC_ANDROID_VERSION_CODE:
+    parseInt(process.env.EXPO_PUBLIC_ANDROID_VERSION_CODE || '') || 1000001
 };
 
 if (process.env.APP_ENV === 'production') {
@@ -18,17 +31,17 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   // Spreads static configuration from app.json if we want to seperate out and be more
   // explicit about what should / and should not change
   ...config,
-  name: process.env.EXPO_PUBLIC_APP_DISPLAY_NAME || '(Dev) RouterWind',
+  name: ENV.EXPO_PUBLIC_APP_DISPLAY_NAME || '(Dev) RouterWind',
   owner: 'rally2',
   slug: 'routerwind',
   description:
     'Expo Router & Nativewind Template to quick project spin up.  Configured environments, EAS profiles & more!',
   privacy: 'hidden',
   platforms: ['android', 'ios', 'web'],
-  scheme: process.env.EXPO_PUBLIC_SCHEME,
-  version: process.env.EXPO_PUBLIC_APP_VERSION,
+  scheme: ENV.EXPO_PUBLIC_SCHEME,
+  version: ENV.EXPO_PUBLIC_APP_VERSION,
   orientation: 'portrait',
-  icon: process.env.EXPO_PUBLIC_ICON_PATH,
+  icon: ENV.EXPO_PUBLIC_ICON_PATH,
   userInterfaceStyle: 'automatic',
   splash: {
     image: './assets/splash.png',
@@ -44,17 +57,17 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   assetBundlePatterns: ['**/*'],
   ios: {
-    bundleIdentifier: process.env.EXPO_PUBLIC_BUNDLE_ID,
-    buildNumber: process.env.EXPO_PUBLIC_BUILD_NUMBER,
+    bundleIdentifier: ENV.EXPO_PUBLIC_BUNDLE_ID,
+    buildNumber: ENV.EXPO_PUBLIC_BUILD_NUMBER,
     supportsTablet: true
   },
   android: {
-    package: process.env.EXPO_PUBLIC_PACKAGE,
+    package: ENV.EXPO_PUBLIC_PACKAGE,
     adaptiveIcon: {
-      foregroundImage: process.env.EXPO_PUBLIC_FOREGROUND_IMAGE,
+      foregroundImage: ENV.EXPO_PUBLIC_FOREGROUND_IMAGE,
       backgroundColor: '#1F2937'
     },
-    versionCode: parseInt(process.env.EXPO_PUBLIC_BUILD_NUMBER || '1')
+    versionCode: ENV.EXPO_PUBLIC_ANDROID_VERSION_CODE || 1000001
   },
   web: {
     bundler: 'metro',
@@ -74,12 +87,28 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   plugins: ['expo-router']
 });
 
-// TODO: Potentially remove having leveraged .env files and eas.json instead
 function setupProductionEnvironment() {
-  ENV.DYNAMIC_CONFIG_ENV_VAR = 'keep it secret, keep it safe... (prod)'
+  ENV.EXPO_PUBLIC_APP_DISPLAY_NAME = 'RouterWind';
+  ENV.EXPO_PUBLIC_PACKAGE = 'com.rally2.routerwind';
+  ENV.EXPO_PUBLIC_FOREGROUND_IMAGE = './assets/adaptive-icon.png';
+  ENV.EXPO_PUBLIC_SCHEME = 'routerwind';
+  ENV.EXPO_PUBLIC_BUNDLE_ID = 'com.rally2.routerwind';
+  ENV.EXPO_PUBLIC_ICON_PATH = './assets/icon.png';
+  ENV.EXPO_PUBLIC_APP_VERSION = '1.0';
+  ENV.EXPO_PUBLIC_BUILD_NUMBER = '1';
+  ENV.EXPO_PUBLIC_OTA = '0';
+  ENV.EXPO_PUBLIC_ANDROID_VERSION_CODE = 1000001;
 }
 
-// TODO: Potentially remove having leveraged .env files and eas.json instead
 function setupStagingEnvironment() {
-  ENV.DYNAMIC_CONFIG_ENV_VAR = 'keep it secret, keep it safe... (staging)'
+  ENV.EXPO_PUBLIC_APP_DISPLAY_NAME = '(Staging) RouterWind';
+  ENV.EXPO_PUBLIC_PACKAGE = 'com.rally2.routerwind.staging';
+  ENV.EXPO_PUBLIC_FOREGROUND_IMAGE = './assets/staging/adaptive-icon.staging.png';
+  ENV.EXPO_PUBLIC_SCHEME = 'routerwindstaging';
+  ENV.EXPO_PUBLIC_BUNDLE_ID = 'com.rally2.routerwind.staging';
+  ENV.EXPO_PUBLIC_ICON_PATH = './assets/staging/icon.staging.png';
+  ENV.EXPO_PUBLIC_APP_VERSION = '1.0';
+  ENV.EXPO_PUBLIC_BUILD_NUMBER = '3';
+  ENV.EXPO_PUBLIC_OTA = '0';
+  ENV.EXPO_PUBLIC_ANDROID_VERSION_CODE = 1000001;
 }
